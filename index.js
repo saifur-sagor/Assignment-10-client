2c2c85b32559993de7dd944161c836f8479eaa13
saifur-sagor <mdsaifurrahmansagor606@gmail.com>
2025-11-10 19:08:07 +0600

    setup

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 4000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://e_learning_db:wfCqXkZE4g1x7Rq2@cluster0.ukfyacf.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running now!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
